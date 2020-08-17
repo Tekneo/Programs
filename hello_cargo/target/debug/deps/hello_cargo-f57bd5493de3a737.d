b640efee36cff94e42d124f421008badc1138604
Nachiketh <technology3.0cradle@gmail.com>
2020-08-17 22:14:33 +0530

    created a project

---
+++ hello_cargo/target/debug/deps/hello_cargo-f57bd5493de3a737.d
@@ -0,0 +1,5 @@
+/home/neoengineer/Tekneo/Programs/hello_cargo/target/debug/deps/hello_cargo-f57bd5493de3a737: src/main.rs
+
+/home/neoengineer/Tekneo/Programs/hello_cargo/target/debug/deps/hello_cargo-f57bd5493de3a737.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/neoengineer/Tekneo/Programs/hello_cargo/target/debug/hello_cargo: /home/neoengineer/Tekneo/Programs/hello_cargo/src/main.rs
