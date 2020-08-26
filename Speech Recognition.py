226515226cddc258e389c80c8e3e874c6946e4bf
Nachiketh <technology3.0cradle@gmail.com>
2020-08-26 21:57:27 +0530

    added a project file

---
+++ Speech Recognition.py
@@ -0,0 +1,12 @@
+from typing import Text
+import speech
+import speech_recognition as sr
+speaker = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Speak Something")
+    audio = speaker.listen(source)
+    try:
+        Text = speaker.recognize_google(audio)
+        print('You said: {}'.format.(Text))
+    except
+        print("Sorry , I could not capture your Voice")
